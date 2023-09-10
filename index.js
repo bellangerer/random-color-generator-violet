4c9aa9683acd6a5cb60764691e398676bcc9f1c9
bellangerer <bella.angerer@gmail.com>
2023-09-10 18:44:28 +0200

    delete yarn.lock

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-// random color
 const color = randomColor({ hue: argv[2], luminosity: argv[3] });
 console.log('hello');
 
-// string part
 console.log(
   chalk.hex(color).bold(`###############################
 ###############################
